d7c1271d3304b5386dac9c937a016e546f399282
Anthony <anthonyheadcase@yahoo.com>
2017-07-03 19:35:05 -0700

    Added Skills Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170704015535) do
-
+ActiveRecord::Schema.define(version: 20170704023151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "experiences", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.string "title"
+    t.string "company"
+    t.text "details"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "skill_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "first_name"
@@ -31,14 +44,6 @@ ActiveRecord::Schema.define(version: 20170704015535) do
     t.string "resume_url"
     t.string "github_url"
     t.string "photo_url"
-
-  create_table "experiences", force: :cascade do |t|
-    t.date "start_date"
-    t.date "end_date"
-    t.string "title"
-    t.string "company"
-    t.text "details"
-
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
