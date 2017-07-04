d77c197ca9275af520935a1f8d9f7f6fafcab004
Calvin <mrcalvinkc@gmail.com>
2017-07-03 21:19:26 -0700

    Added the create method and its json view

--- app/controllers/api/v1/students_controller.rb
+++ app/controllers/api/v1/students_controller.rb
@@ -10,8 +10,25 @@ class Api::V1::StudentsController < ApplicationController
   end
 
   def create
-    
-    
+    @student = Student.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
+      phone_number: params[:phone_number],
+      short_bio: params[:short_bio],
+      linkedin_url: params[:linkedin_url],
+      twitter_handle: params[:twitter_handle],
+      website_url: params[:website_url],
+      resume_url: params[:resume_url],
+      github_url: params[:github_url],
+      photo_url: params[:photo_url]
+    )
+    if @student.save
+      render 'show.json.jbuilder'
+    else 
+      render json: {errors: @student.errors.full_messages}, status: 422
+    end
   end
 
   def update
