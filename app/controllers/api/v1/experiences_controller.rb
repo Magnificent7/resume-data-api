9eaf751a2d73a52fa281ce903cc5479cefccba6f
Calvin <mrcalvinkc@gmail.com>
2017-07-05 19:11:52 -0700

    Added the student_id foreign key to student's has_many models

---
+++ app/controllers/api/v1/experiences_controller.rb
@@ -0,0 +1,62 @@
+class Api::V1::ExperiencesController < ApplicationController
+  def index
+    @experiences = Experience.all
+    render 'index.json.jbuilder'
+  end
+
+  def show
+    @experiences = Student.find(params[:id])
+    render 'show.json.jbuilder'
+  end
+
+  def create
+    @student = Student.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
+      phone_number: params[:phone_number],
+      short_bio: params[:short_bio],
+      linkedin_url: params[:linkedin_url],
+      twitter_handle: params[:twitter_handle],
+      website_url: params[:website_url],
+      resume_url: params[:resume_url],
+      github_url: params[:github_url],
+      photo_url: params[:photo_url]
+    )
+    if @student.save
+      render 'show.json.jbuilder'
+    else 
+      render json: {errors: @student.errors.full_messages}, status: 422
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @student = Student.find(params[:id])
+      @student.update(
+        first_name: params[:first_name],
+        last_name: params[:last_name],
+        email: params[:email],
+        password: params[:password],
+        phone_number: params[:phone_number],
+        short_bio: params[:short_bio],
+        linkedin_url: params[:linkedin_url],
+        twitter_handle: params[:twitter_handle],
+        website_url: params[:website_url],
+        resume_url: params[:resume_url],
+        github_url: params[:github_url],
+        photo_url: params[:photo_url]
+    )
+    render 'show.json.jbuilder'
+
+  end
+
+  def destroy
+    student = Student.find(params[:id])
+    student.destroy
+    head :no_content, status: 204
+  end
+end
